3949f8895cbb63583bef70a1a6f8411927812b7c
betinsk <gmdust@hotmail.com>
2024-02-29 19:25:47 -0300

    portifolio

--- src/api/arrayProjects.js
+++ src/api/arrayProjects.js
@@ -1,12 +1,15 @@
 
 import pureform from '../assets/pureform.png'
+import prisaofederal from '../assets/prisaofederal.png'
+import carrinhoCompras from '../assets/carrinhoCompras.png'
+
 
 const ArrayProjects = async () => {
 const projectsData = [
         {
           id: 1,
           name: 'Prisão federal',
-          img: pureform,
+          img: prisaofederal,
           description: 'Um sistema de prisão federal Americano, inspirado na serie de tv, Orange is the new black',
           link: 'https://prisaofederal.netlify.app/'
         },
@@ -16,6 +19,13 @@ const projectsData = [
           img: pureform,
           description: 'Um sistema de academia, feito em javaScript',
           link: 'https://pureformfitness.netlify.app/index.html'
+        },
+        {
+          id: 3,
+          name: 'Carrinho de compras Mercado Livre',
+          img: carrinhoCompras,
+          description: 'Carrinho de compras feito em react, baseado numa api do Mercado Livre',
+          link: 'https://carrinhocomprasbetinsk.netlify.app/'
         },
         // Adicione mais objetos de projeto conforme necessário
       ];
--- src/components/projects.js
+++ src/components/projects.js
@@ -1,7 +1,6 @@
 // Projects.js
 import React, { useState } from 'react';
 import '../css/project.css'
-import pureform from '../assets/pureform.png'
 import { Link } from 'react-router-dom'
 import { useEffect } from 'react';
 import ArrayProjects from '../api/arrayProjects';
@@ -26,7 +25,7 @@ function Projects() {
         {projectData.map((project) => (
           <div className="project-card" key={project.id}>
             <h3>{project.name}</h3>
-            <img src= {pureform} />
+            <img src= {project.img} />
             <p>{project.description}</p>
             <Link to={`/projectDetails/${project.id}`}> Detalhes</Link>
           </div>
